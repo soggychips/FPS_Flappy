e0c387e2b3cd2f0d4617b4da635b3e0273c34bd5
Jonathan Banashek <jbanashek@outlook.com>
2014-02-18 17:11:31 -0800

    Basic UI
    Basic gui elements added for before game start and score after death

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Engine : MonoBehaviour {
 	public GameObject camera,light,bird,floor,background,pipeCreator;
+	bool isNotStarted = true;
+	bool isDead = false;
 	// Use this for initialization
 	void Start () {
 		Instantiate(camera);
@@ -16,5 +18,20 @@ public class Engine : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		//guitesting stuff
+		if(Input.GetKeyDown(KeyCode.G)) {
+			isNotStarted = !isNotStarted;
+			isDead = !isDead;
+		}
+	}
+
+	void OnGUI () {
+		if (isNotStarted) {
+			//make gui box for the tap to move and other pre-start items	
+			GUI.Box (new Rect ((Screen.width / 3), (Screen.height / 3), (Screen.width / 3), (Screen.height / 3)), new GUIContent ("Press Space to Jump"));
+		} else if (isDead) {
+			//show score screen gui
+			GUI.Box (new Rect ((Screen.width / 3), (Screen.height / 3), (Screen.width / 3), (Screen.height / 3)), new GUIContent ("Score: "));
+		}
 	}
 }
